5ae58c51adbf203639633ba8f18b44a71542c484
mohamadjaved <maj38@njit.edu>
2018-11-27 20:09:25 -0500

    Created a unit test to update the name of a user in the database to Steve Smith.

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -14,12 +14,23 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    /* public function testExample()
+     {
+         $user = new User();
+         $user->name ='Mohamad';
+         $user->email ='maj38@njit.edu';
+         $user->password='password';
+         $this->assertTrue($user->save());
+     }*/
+
+    public function testUpdateUserName()
     {
-        $user = new User();
-        $user->name ='Mohamad';
-        $user->email ='maj38@njit.edu';
-        $user->password='password';
+        $user = User::all()->last();
+
+        //dd($user);
+        //$user->name="Steve Smith"; //To assign a name and update it using update()
+        //$this->assertTrue($user->update());
+        $user->where('name', 'Mohamad')->update(['name' => 'Steve Smith']);
         $this->assertTrue($user->save());
     }
 }
